45654b4aa5875375c849b00b13b2a3db8faf02aa
张苏 <zhangsu@zhangsus-Air.lan>
2025-01-03 04:12:24 +1030

    init

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import { Phone, Mail, MapPin } from 'lucide-react'
 
 export default function Contact() {
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import { BookOpen, Users, Briefcase } from 'lucide-react'
 
 export default function OurServices() {
